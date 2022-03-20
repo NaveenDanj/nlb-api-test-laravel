ea2ba321f66a9807c9337dafaf4718c4d284a63a
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-20 22:52:59 +0530

    added login endpoint

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
-class UserAuthController extends Controller
-{
+class UserAuthController extends Controller{
+
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken('default')->plainTextToken;
+
+    }
 
 }
