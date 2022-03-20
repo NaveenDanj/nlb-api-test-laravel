a8a64be5bb646718cdcf23887f8789bd0e2ca5b1
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-20 22:50:56 +0530

    added user auth controller and auth routes

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\UserAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+//user auth routes
+Route::prefix('auth')->group(function () {
+    Route::post('login', [UserAuthController::class , 'login']);
+    Route::post('register', 'UserAuthController@register');
+    Route::post('logout', 'UserAuthController@logout');
+    Route::post('me', 'UserAuthController@me');
+});
